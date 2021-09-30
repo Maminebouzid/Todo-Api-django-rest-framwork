d0cebd2ef22e7060add45747da563a1a99de6f66
amine <aminebzd33@gmail.com>
2021-10-01 00:13:31 +0100

    working project

--- apis/tests.py
+++ apis/tests.py
@@ -7,6 +7,8 @@ from rest_framework.test import APITestCase
 from todos.models import Todo
 
 class AccountTests(APITestCase):
+    def setUp(self):
+        Todo.objects.create(id=4,title='test',description='test')
     def test_create_todo(self):
         """
         Ensure we can create a new account object.
@@ -32,4 +34,11 @@ class AccountTests(APITestCase):
         data = {'title': 'unit test' , 'description':'check if test work well'}
         response = self.client.put('apis/v1/todo/5/',data,format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_delete(self):
+        """
+        ensure that  we can delete content , notice that 204 flag mean that operation succeeded
+        """
+        url = reverse('todos-detail', kwargs={'pk': 4})
+        response = self.client.delete(url,format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
